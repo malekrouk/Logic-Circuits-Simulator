cc58dfb029b0b23b9c621251f86638dc060da71f
mostafa21314 <123311211+mostafa21314@users.noreply.github.com>
2024-03-14 22:56:41 +0200

    Update Source.cpp to test for the new function reading stim files

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,8 @@ int main()
 	FilesReading test;
 	string libFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/TestCircuit_2/Circuit_2_LIB.lib";
 	string cirFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/TestCircuit_2/Circuit_2_CIR.cir";
+	string stimFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/TestCircuit_2/Circuit_2_STIM.stim";
+
 	ifstream hh;
 	test.ReadLibFile(libFileName);
 
@@ -15,5 +17,25 @@ int main()
 	//	cout << test.Lib.InputNum[i] << endl;
 	//}
 	test.ReadCirFile(cirFileName);
+
+	test.ReadStimFile(stimFileName);
+
+
+	/*for (int i = 0; i < test.stim.Delay.size(); i++)
+	{
+		cout << test.stim.Delay[i]<<endl;
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < test.cir.Inputs.size(); i++)
+	{
+		cout << test.cir.Inputs[i]<<endl;
+	}
+	cout << endl << endl;
+	for (int i = 0; i < test.cir.componentName.size(); i++)
+	{
+		cout << test.cir.componentName[i]<<endl;
+	}*/
 
 }
