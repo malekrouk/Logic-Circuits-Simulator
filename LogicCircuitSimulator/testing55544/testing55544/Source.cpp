6f23f0a9e7444eb48d7bcbd78272dc676265d651
mostafa21314 <123311211+mostafa21314@users.noreply.github.com>
2024-03-28 09:21:31 +0200

    Update Source.cpp

--- LogicCircuitSimulator/testing55544/testing55544/Source.cpp
+++ LogicCircuitSimulator/testing55544/testing55544/Source.cpp
@@ -28,26 +28,46 @@ bool evaluateGate(string s, vector<bool> inp)    //functiont that uses the gate 
 
 
 
-int main()
-{
+//int main(int argc, char *argv[])
+//{
+//    if(argc != 5) {
+//        cout << "Usage: " << argv[0] << " <library file> <circuit file> <stimuli file> <output file>" << endl;
+//        return 1;
+//    }
+//
+//    string libFileName = argv[1];
+//    string cirFileName = argv[2];
+//    string stimFileName = argv[3];
+//    string OutName = argv[4]; // Output file path is now a command line argument
+//
+//    FilesReading test;
+//
+//    test.ReadLibFile(libFileName);
+//    test.ReadCirFile(cirFileName);
+//    test.ReadStimFile(stimFileName);
 
 
-	FilesReading test;
-	string libFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/cells.lib";    //file locations, make sure you change them to work on your machine
-	string cirFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/1.cir";
-	string stimFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/2.stim";
-	string OutName = "C:/Users/mosta/Desktop/AUC/DD1/Project/Circuit_output.sim";      //the sim file that will be produced
+	int main()
+	{
+
+
+		FilesReading test;
+		string libFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/cells.lib";    //file locations, make sure you change them to work on your machine
+		string cirFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/1.cir";
+		string stimFileName = "C:/Users/mosta/Desktop/AUC/DD1/Project/2.stim";
+		string OutName = "C:/Users/mosta/Desktop/AUC/DD1/Project/Circuit_output.sim";      //the sim file that will be produced
 
 
 
 
-	test.ReadLibFile(libFileName);
-	test.ReadCirFile(cirFileName);    //here the readFile class starts reading the files and storing data
-	test.ReadStimFile(stimFileName);
+		test.ReadLibFile(libFileName);
+		test.ReadCirFile(cirFileName);    //here the readFile class starts reading the files and storing data
+		test.ReadStimFile(stimFileName);
 
 
-	//test.PrintLib();
-	//cout << endl << endl;
+
+	test.PrintLib();
+	cout << endl << endl;
 	test.PrintCir();    //part of the debugging process, functions that displays whats stored in each file struct
 	cout << endl << endl;
 	test.PrintStim();
